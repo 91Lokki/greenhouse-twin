5221eb933ef68979e6c0a92c53723bf277ddd31f
Mike <smilemike0906@gmail.com>
2026-03-24 20:37:52 +0800

    Initial Commit

---
+++ GreenhouseTwin/GreenhouseTwinApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenhouseTwinApp.swift
+//  GreenhouseTwin
+//
+//  Created by lokki on 2026/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GreenhouseTwinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
